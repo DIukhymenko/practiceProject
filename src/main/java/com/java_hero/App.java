0f1c2103fff3dc9a21b24c5912a88d1ab6e83126
Dmytro Iukhymenko <diukh@softserveinc.com>
2018-09-05 18:09:43 +0300

    Configure full execution of simple WEB test

--- src/main/java/com/java_hero/App.java
+++ src/main/java/com/java_hero/App.java
@@ -1,7 +1,13 @@
 package com.java_hero;
 
+import io.github.tatools.sunshine.core.RegexCondition;
+import io.github.tatools.sunshine.core.Sun;
+import io.github.tatools.sunshine.core.VerboseRegex;
+import io.github.tatools.sunshine.testng.LoadableTestNGSuite;
+import io.github.tatools.sunshine.testng.TestNGKernel;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new Sun(new TestNGKernel(new LoadableTestNGSuite(new VerboseRegex(new RegexCondition("^com.java_hero.tests(.+)?"))))).shine();
     }
 }
